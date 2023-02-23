e2128f24be9ef2463898362563d4721115639c20
KensukeNakazawa <infokensuke.n@gmail.com>
2023-02-23 12:23:27 +0900

    :sparkles: Implement GetPearVersions Handler

---
+++ api/http/handler/pear_data_test.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+	"api/domain/presenter"
+	"api/usecase"
+)
+
+func TestPearData_GetPearVersions(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/pears", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	ctrl := gomock.NewController(t)
+	mockPearUseCase := usecase.NewMockPear(ctrl)
+
+	mockPearDataVersionOutputs := make([]presenter.PearDataVersionOutput, 2)
+	mockPearDataVersionOutputs[0] = presenter.PearDataVersionOutput{
+		Version:     "1.0.0",
+		ReleaseNote: "release",
+		CreatedAt:   time.Now(),
+	}
+	mockPearDataVersionOutputs[1] = presenter.PearDataVersionOutput{
+		Version:     "1.0.1",
+		ReleaseNote: "release",
+		CreatedAt:   time.Now(),
+	}
+	mockPearUseCase.EXPECT().GetDataVersions().Return(mockPearDataVersionOutputs, nil)
+	handler := NewPearData(mockPearUseCase)
+
+	err := handler.GetPearVersions(c)
+	assert.Nil(t, err)
+}

b0a7388f76e9e1c593f5699854caeb979ba22f01
KensukeNakazawa <infokensuke.n@gmail.com>
2023-03-15 16:24:45 +0900

    :ok_hand: small fix

--- frontend/src/components/features/introduce/Main.tsx
+++ frontend/src/components/features/introduce/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import pearExample1 from 'images/example_1.png';
 import pearExample2 from 'images/example_2.png';
 import 'components/features/introduce/Main.css'
-import { Box, Container, Link, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import SmartLifeCharacter from '../../ui/SmartLifeCharacter';
 
 const Introduce = () => {
@@ -29,15 +29,12 @@ const Introduce = () => {
         <SmartLifeCharacter/>
 
         <Typography variant="subtitle1" align="left" color="text.secondary" paragraph>
-          　新潟県の特産品の一つである高級洋梨「ル レクチエ」は、形状や外観などに関する出荷規格に基づいて
-          等級判定が行われています。現在、多くの場合この等級判定は生産者の目視により行われており、作業負担と
-          判定における個人差の発生が課題となっています。新潟大学工学部山﨑研究室では、これまでル レクチエの
-          外観品質の自動評価の研究を進め、特に深層学習を利用した外観汚損検出から等級判定まで行うシステムを
-          提案しており、その過程でル レクチエの外観画像のデータセットを構築してきました。
+          　新潟県の特産品の一つである高級洋ナシ「ル レクチエ」は、形状や外観などに関する出荷規格に基づいて等級判定が行われています。
+          現在、多くの場合この等級判定は生産者の目視により行われており、作業負担と判定における個人差の発生が課題となっています。
+          新潟大学工学部山﨑研究室では、これまでル レクチエの外観品質の自動評価の研究を進め、特に深層学習を利用した外観汚損検出から等級判定まで行うシステムを提案しており、その過程でル レクチエの外観画像のデータセットを構築してきました。
           <br/>
-          　農業分野での深層学習技術の適用が進められては来ているものの、データセット構築に専門家の知識が必要であることや
-          作物ごとに学習に適した量のデータが必要であること等が、実用化へのボトルネックになっています。
-          この課題の克服へ貢献するために、当研究室で構築したル レクチエのデータセットを公開し、農業分野での深層学習の実用化がより進展することを期待します。
+          　農業分野での深層学習技術の適用が進められては来ているものの、データセット構築に専門家の知識が必要であることや作物ごとに学習に適した量のデータが必要であること等が、実用化へのボトルネックになっています。
+          この課題の克服へ貢献するために、当研究室では構築したル レクチエのデータセットを公開し、農業分野での深層学習の実用化がより進展することを期待します。
         </Typography>
       </Container>
 
@@ -53,14 +50,20 @@ const Introduce = () => {
       >
         公開データの特徴
       </Typography>
-      <Typography variant="subtitle1" align="center" color="text.secondary" paragraph>
-        <div>
-          新潟県名産品のル レクチエの外観品質を定義したデータの公開
-        </div>
-        <div>
-          外観品質を定義した独自フォーマットと物体検出時の一般的なアノテーションフォーマット(COCO)により提供
-        </div>
+      <Typography variant="subtitle1" align="left" color="text.secondary" paragraph>
+        <ol>
+          <li>
+            1. 新潟県名産品のル レクチエの外観品質を定義したデータの公開
+          </li>
+          <li>
+            2. 外観品質を定義した独自フォーマットと物体検出時の一般的なアノテーションフォーマット(COCO)により提供
+          </li>
+          <li>
+            3. 1000枚を超えるアノテーション済み画像データの提供
+          </li>
+        </ol>
       </Typography>
+
       <ul>
         <li><img src={pearExample1} className="pear-example-item" alt="pear_example_1" /></li>
         <li><img src={pearExample2} className="pear-example-item" alt="pear_example_2" /></li>
--- frontend/src/components/features/version/Term.tsx
+++ frontend/src/components/features/version/Term.tsx
@@ -18,14 +18,15 @@ export default function Term() {
           overflowY: "scroll",
         }}
         >
-        <Typography paragraph>
+
+        <Typography align="left" paddingLeft="45px" paragraph>
           この利用規約（以下、「本規約」といいます。）は、新潟大学山﨑達也研究室（以下、「当研究室」）がウェブサイト上で提供するサービス「PearOpenData」（以下、「本サービス」）の提供条件と、
           本サービスを利用するユーザーに遵守していただくべき事項、運営元とユーザーの間の権利義務関係が定められています。
         </Typography>
 
         <div>
           <Typography component="h5" variant="subtitle1">第1条（適用）</Typography>
-          <Typography paragraph>
+          <Typography align="left" paragraph>
             <ol>
               <li>本規約は、本サービスの提供条件と本サービスの利用に関する当研究室とユーザーとの間の権利義務関係を定めることを目的として、ユーザーと当研究室との間の本サービスの利用に関わるすべての関係に適用されます。</li>
               <li>本規約のほか、当研究室が定め、本サービス上に掲載する本サービスの利用に関するルール、各種規定は、本規約の一部を構成するものとします。</li>
@@ -36,9 +37,9 @@ export default function Term() {
 
       <div>
         <Typography component="h5" variant="subtitle1">第2条（禁止事項）</Typography>
-        <Typography paragraph>
-          ユーザーは、本サービスの利用にあたり、以下の行為をしてはなりません。
+        <Typography align="left" paragraph>
           <ol>
+            ユーザーは、本サービスの利用にあたり、以下の行為をしてはなりません。
             <li>法令または公序良俗に違反する行為</li>
             <li>犯罪行為に関連する行為</li>
             <li>本サービスの内容等、本サービスに含まれる著作権、商標権ほか知的財産権を侵害する行為</li>
@@ -59,7 +60,7 @@ export default function Term() {
 
       <div>
         <Typography component="h5" variant="subtitle1">第3条（本サービスの提供の停止等）</Typography>
-        <Typography paragraph>
+        <Typography align="left" paragraph>
           当研究室は、以下のいずれかの事由があると判断した場合、ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
           <ol>
             <li>本サービスにかかるコンピュータシステムの保守点検または更新を行う場合</li>
@@ -73,7 +74,7 @@ export default function Term() {
 
       <div>
         <Typography component="h5" variant="subtitle1">第4条（保証の否認および免責事項）</Typography>
-        <Typography paragraph>
+        <Typography align="left" paragraph>
           当研究室は、本サービスに事実上または法律上の瑕疵（安全性、信頼性、正確性、完全性、有効性、特定の目的への適合性、セキュリティなどに関する欠陥、エラーやバグ、権利侵害などを含みます。）がないことを明示的にも黙示的にも保証しておりません。<br/>
           当研究室は、本サービスに起因してユーザーに生じたあらゆる損害について一切の責任を負いません。<br/>
           前項ただし書に定める場合であっても、当研究室は、当研究室の過失（重過失を除きます。）による債務不履行または不法行為によりユーザーに生じた損害のうち特別な事情から生じた損害（当研究室またはユーザーが損害発生につき予見し、または予見し得た場合を含みます。）について一切の責任を負いません。<br/>
@@ -84,14 +85,14 @@ export default function Term() {
 
       <div>
         <Typography component="h5" variant="subtitle1">第5条（サービス内容の変更等）</Typography>
-        <Typography paragraph>
+        <Typography align="left" paragraph>
           当研究室は、ユーザーに通知することなく、本サービスの内容を変更しまたは本サービスの提供を中止することができるものとし、これによってユーザーに生じた損害について一切の責任を負いません。
         </Typography>
       </div>
 
       <div>
         <Typography component="h5" variant="subtitle1">第6条（利用規約の変更）</Typography>
-        <Typography paragraph>
+        <Typography align="left" paragraph>
           当研究室は、必要と判断した場合には、ユーザーに通知することなくいつでも本規約を変更することができるものとします。<br/>
           なお、本規約の変更後、本サービスの利用を開始した場合には、当該ユーザーは変更後の規約に同意したものとみなします。
         </Typography>
@@ -101,7 +102,7 @@ export default function Term() {
         <Typography component="h5" variant="subtitle1">
           第7条（通知または連絡）
         </Typography>
-        <Typography paragraph>
+        <Typography align="left" paragraph>
           ユーザーと当研究室との間の通知または連絡は、当研究室の定める方法によって行うものとします。<br/>
           当研究室は、ユーザーから、当研究室が別途定める方式に従った変更届け出がない限り、現在登録されている連絡先が有効なものとみなして当該連絡先へ通知または連絡を行い、これらは、発信時にユーザーへ到達したものとみなします。
         </Typography>
@@ -109,19 +110,19 @@ export default function Term() {
 
       <div>
         <Typography component="h5" variant="subtitle1">第8条（個人情報の取り扱い）</Typography>
-        <Typography paragraph></Typography>
+        <Typography align="left" paragraph></Typography>
       </div>
 
       <div>
         <Typography component="h5" variant="subtitle1">第9条（使用許諾）</Typography>
-        <Typography paragraph></Typography>
+        <Typography align="left" paragraph></Typography>
       </div>
 
       <div>
         <Typography component="h5" variant="subtitle1">第10条（準拠法・裁判管轄）</Typography>
-        <Typography paragraph>
+        <Typography align="left" paragraph>
           本規約の解釈にあたっては、日本法を準拠法とします。
-          本サービスに関して紛争が生じた場合には、当研究室の本店所在地を管轄する裁判所を専属的合意管轄とします。
+          本サービスに関して紛争が生じた場合には、当研究室の所在地を管轄する裁判所を専属的合意管轄とします。
         </Typography>
       </div>
     </Box>
